2eb1732d27e234af1994710867dfbfb4a2a9f41c
Brian <galyenb@vcu.edu>
2023-09-07 11:05:36 -0400

    added the start btn and general blueprint in JS

---
+++ assets/script.js
@@ -0,0 +1,36 @@
+// let createTag = document.createElement();
+
+// directions on how to play the game
+
+// start button
+function createStartButton() {
+  const genBtn = document.createElement("button");
+
+  console.log(genBtn);
+
+  genBtn.textContent = "Start!";
+
+  genBtn.setAttribute("id", "start");
+
+  document.body.appendChild(genBtn);
+
+  // start button styling
+  genBtn.setAttribute("style");
+}
+createStartButton();
+
+// start button functionality
+
+function startGame() {
+  const start = document.getElementById(start);
+  start.onclick;
+}
+// high score display
+
+// timer display and functions
+
+// Question Container
+
+// Choice Container
+
+// Enter Initials for High Score
